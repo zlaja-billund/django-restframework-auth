2521fae1b2206afd1c0b9a0000fe8dbc0ebccedf
zlaja-billund <mail@zlatan.dk>
2024-11-26 17:03:35 +0100

    login removed unused imports

--- users/views.py
+++ users/views.py
@@ -3,8 +3,6 @@ from rest_framework.response import Response
 import rest_framework.status as status
 from rest_framework.views import APIView
 from rest_framework.authtoken.models import Token
-from rest_framework import generics
-from django.contrib.auth.models import User
 
 from .serializers import UserSerializer
 
